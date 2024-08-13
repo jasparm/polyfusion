22ebcd385056bd1f36292908cd20bb3a6f9ebbd2
Shannon Johnson <sjoh0041@student.monash.edu>
2024-08-13 16:33:08 +1000

    Add function to find closest vertex to a point in 3D space.

--- client/src/lib/MeshHelper.ts
+++ client/src/lib/MeshHelper.ts
@@ -19,3 +19,37 @@ export function getCentrePoint(mesh: THREE.Mesh): THREE.Vector3 {
     return center;
 
 }
+
+/**
+ * Finds the closest vertex in a list of vertices from a point.
+ * 
+ * @param vertices An array of Vector3 objects representing a vertex in 3D space.
+ * @param position A Vector3 object representing a point in 3D space.
+ * @returns The index of the vertex in the vertices array.
+ */
+export function findClosestVertex(vertices: THREE.Vector3[], position: THREE.Vector3): number | null {
+    var closestVertex: number | null = null;
+    var minDistance = Infinity;
+
+    // Calculate distance from position to all vertices
+    for (let i = 0; i < vertices.length; i ++){
+        const vertex = vertices[i]
+        // Calculate Euclidean distance between current vertex and position
+        const distance = Math.sqrt(
+            Math.pow(vertex.x - position.x, 2) +
+            Math.pow(vertex.y - position.y, 2) +
+            Math.pow(vertex.z - position.z, 2)
+        );
+
+        // If distance is the smallest we have seen so far, update min distance and closest vertex
+        if (distance < minDistance) {
+            minDistance = distance;
+            closestVertex = i;
+        }
+    }
+    vertices.forEach(vertex => {
+        
+    });
+
+    return closestVertex;
+}
