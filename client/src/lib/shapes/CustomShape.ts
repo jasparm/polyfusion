3303b703d0c2836a44b99bda2c6f61c7dd61eedd
Shannon Johnson <sjoh0041@student.monash.edu>
2024-08-14 21:35:20 +1000

    Add custom shapes and shape modification
    Added the ability to define custom shapes by defining vertex locations and which vertexes are connected. Also added ability for user to add new vertices to pre-existing shape dynamically.

--- client/src/lib/shapes/CustomShape.ts
+++ client/src/lib/shapes/CustomShape.ts
@@ -1,7 +1,12 @@
 import * as THREE from "three";
 
 import { getCentrePoint } from "../MeshHelper.ts";
-import { computeNormal } from "./ShapeHelpers.ts";
+import {
+  checkForVerticesWithSameDistance,
+  computeNormal,
+  findClosestTwoVertices,
+  generateCombinations,
+} from "./ShapeHelpers.ts";
 import { collidesWith } from "../Collision.ts";
 
 export class CustomShape {
@@ -13,7 +18,8 @@ export class CustomShape {
   geometry: THREE.PolyhedronGeometry; // Stores the geometry of the shape
   material: THREE.Material; // Stores the material used on the shape
   mesh: THREE.Mesh; // Stores the combined mesh which can be added to the scene.
-
+  group: THREE.Group; // Stores all other shapes that are part of the mesh.
+  userVertices: number[]; // Stores user defined vertex information.
   radius: number;
 
   constructor(
@@ -22,17 +28,26 @@ export class CustomShape {
     connections: number[][] = [],
     radius: number = 1
   ) {
-    this.vertices = this.mapVerticesToVector3(vertices);
     this.connections = connections;
     this.scene = scene;
     this.radius = radius;
+    this.group = new THREE.Group();
+    this.userVertices = vertices;
 
+    this.init();
+
+    // this.group.add(this.mesh);
+  }
+
+  private init() {
     this.geometry = new THREE.PolyhedronGeometry(
-      vertices,
+      this.userVertices,
       this.convertConnections(this.connections),
       this.radius,
       0
     );
+    this.vertices = this.mapVerticesToVector3();
+
     this.material = new THREE.MeshStandardMaterial({
       color: 0xff00ff,
       side: THREE.DoubleSide,
@@ -40,6 +55,9 @@ export class CustomShape {
     // @ts-ignore Typescript does not like material being passed here even though it works.
     this.mesh = new THREE.Mesh(this.geometry, this.material);
 
+    this.addSpheresToVertices();
+
+    return;
   }
 
   /**
@@ -50,7 +68,6 @@ export class CustomShape {
    */
   isConvex(): boolean {
     const point = getCentrePoint(this.mesh);
-    console.log(this.connections);
 
     this.connections.forEach((connection) => {
       const vertex1 = this.vertices[connection[0]];
@@ -93,29 +110,176 @@ export class CustomShape {
     return faces;
   }
 
-  mapVerticesToVector3(vertices: number[]): THREE.Vector3[] {
-    console.log(vertices);
+  /**
+   * Maps vertices array to array of Vector3. This is much easier to work with to distinguish between each vertex.
+   * Takes the vertex positions from the geometry.
+   *
+   * @returns New array containing Vector3 elements that represent each vertex.
+   */
+  mapVerticesToVector3(): THREE.Vector3[] {
     var vertexes: THREE.Vector3[] = [];
+    const vertices = this.geometry.attributes.position.array;
 
     for (let i = 0; i < vertices.length; i += 3) {
-      console.log(vertices[i]);
-      vertexes.push(new THREE.Vector3(vertices[0], vertices[1], vertices[2]));
+      vertexes.push(
+        new THREE.Vector3(vertices[i], vertices[i + 1], vertices[i + 2])
+      );
     }
 
     return vertexes;
   }
 
-  mapVector3ToVertices() {
+  /**
+   * Converts array of Vector3 back to array of vertices.
+   * This is needed as THREE.JS expects the array to be of this type and not to be of type Vector3.
+   * NOTE: Might not actually be needed, will delete later if not needed.
+   *
+   * @returns Converted Vector3 array.
+   */
+  mapVector3ToVertices(vertexes: THREE.Vector3[]): number[] {
     var vertices: number[] = [];
-
-    this.vertices.forEach((vertex) => {
+    // use instances stored vertices array and create a new array.
+    // this way we can maintain the instances vertices array.
+    vertexes.forEach((vertex) => {
       vertices.push(vertex.x, vertex.y, vertex.z);
     });
 
     return vertices;
   }
 
-  addVertex(point: THREE.Vector3) {}
+  /**
+   * Adds little spheres to the corners of the shape.
+   *
+   * @param radius Radius of the balls. Defaults to 0.1.
+   * @param colour Colour of the balls. Defaults to black.
+   */
+  addSpheresToVertices(
+    radius: number = 0.1,
+    colour: THREE.Color = new THREE.Color(0x000000)
+  ) {
+    const ball = new THREE.SphereGeometry(radius);
+    const ball_material = new THREE.MeshBasicMaterial({ color: colour });
+    const ball_mesh = new THREE.Mesh(ball, ball_material);
+
+    this.vertices.forEach((vertex) => {
+      const newBall = ball_mesh.clone();
+      this.group.add(newBall);
+      // @ts-ignore Typescript keeps thinking that meshes do not have a position attribute when they simply just do.
+      newBall.position.x = vertex.x;
+      // @ts-ignore Typescript keeps thinking that meshes do not have a position attribute when they simply just do.
+      newBall.position.y = vertex.y;
+      // @ts-ignore Typescript keeps thinking that meshes do not have a position attribute when they simply just do.
+      newBall.position.z = vertex.z;
+    });
+  }
+
+
+  /**
+   * Adds a vertex to the CustomShape.
+   * 
+   * @param point - The THREE.Vector3 representing the coordinates of the vertex to be added.
+   */
+  addVertex(point: THREE.Vector3) {
+    // Find the two closest vertices to the point being added
+    const closest = findClosestTwoVertices(this.vertices, point);
+
+    // Convert user-defined vertex coordinates to THREE.Vector3 objects.
+    const userVerticesVector3: THREE.Vector3[] = [];
+    for (let i = 0; i < this.userVertices.length; i += 3) {
+      const newVector = new THREE.Vector3(
+        this.userVertices[i],
+        this.userVertices[i + 1],
+        this.userVertices[i + 2]
+      );
+      userVerticesVector3.push(newVector);
+    }
+    // Check if there are any other vertices with the same distance from the point being added.
+    // This way we can connect more than 1 edge at a time.
+    const validVertices = checkForVerticesWithSameDistance(
+      userVerticesVector3,
+      point
+    );
+    // Determine which vertices wil be used to form connections.
+    var connections: number[];
+    if (validVertices.length > 2) { // Use multiple edges if we can
+      connections = validVertices;
+    } else {
+      connections = closest; //Otherwise just use the two closest vertices.
+    }
+
+    const oldGeometry = this.geometry; // if we need to revert back to the old geometry.
+
+    const vertices = [...this.userVertices]; // Add the new vertex to the list of vertices
+    vertices.push(point.x, point.y, point.z);
+
+    // Validate that the connections are valid. Return if they are not.
+    if (connections[0] === -Infinity || connections[1] === -Infinity) {
+      console.error("No valid vertices to add to");
+      return;
+    }
+    
+    // Convert connections to a flat array
+    const newConnections = this.convertConnections(this.connections);
+
+    const newIndex = vertices.length / 3 - 1; // Index for the new vertex to take.
+
+    const combinations = generateCombinations(validVertices, 2); // Generate all possible combinations of two valid vertices.
+
+    for (let i = 0; i < combinations.length; i++) {
+      newConnections.push(combinations[i][0], combinations[i][1], newIndex);
+    }
+
+    // Check connections and vertexes to make sure they are valid numbers.
+    vertices.forEach((vertex) => {
+      if (isNaN(vertex)) {
+        console.error("Invalid vertex:", vertex);
+        return;
+      }
+    });
+    newConnections.forEach((connection) => {
+      if (isNaN(connection)) {
+        console.error("Invalid connection:", connection);
+        return;
+      }
+    });
+
+    // Create the new geometry with updated vertices and connections.
+    const newGeometry = new THREE.PolyhedronGeometry(
+      vertices,
+      newConnections,
+      this.radius,
+      0
+    );
+
+
+    // @ts-ignore Typescript does not like material being passed here even though it works.
+    this.mesh = new THREE.Mesh(newGeometry, this.material);
+
+    // If the new shape is not convex, revert back to the old geometry.
+    if (!this.isConvex()) {
+      // If the newly created shape is not convex, do not change the shape.
+      // @ts-ignore Typescript does not like material being passed here even though it works.
+      this.mesh = new THREE.Mesh(oldGeometry, this.material);
+      return;
+    }
+
+    // @ts-ignore Typescript does not like material being passed here even though it works.
+    this.geometry = newGeometry;
+    this.userVertices = vertices;
+    this.connections = [];
+    // Convert connections array to be right form factor.
+    for (let j = 0; j < newConnections.length; j += 3) {
+      this.connections.push([
+        newConnections[j],
+        newConnections[j + 1],
+        newConnections[j + 2],
+      ]);
+    }
+
+    // Add the mesh to the group and reinitialise the shape.
+    this.init();
+    this.group.add(this.mesh);
+  }
 
   updateMesh() {}
 }
--- client/src/lib/shapes/ShapeHelpers.ts
+++ client/src/lib/shapes/ShapeHelpers.ts
@@ -10,64 +10,176 @@ import * as THREE from "three";
 export function findClosestVertex(
   vertices: THREE.Vector3[],
   position: THREE.Vector3
-): number | null {
-  var closestVertex: number | null = null;
+): { index: number; distance: number } {
+  var closestVertex: number = -Infinity;
   var minDistance = Infinity;
 
   // Calculate distance from position to all vertices
   for (let i = 0; i < vertices.length; i++) {
-    const distance = calculateDistance(vertices[i]);
+    const distance = calculateDistance(vertices[i], position);
     // If distance is the smallest we have seen so far, update min distance and closest vertex
     if (distance < minDistance) {
       minDistance = distance;
       closestVertex = i;
     }
   }
-  return closestVertex;
 
-  // Calculates the Euclidean distance between two points.
-  // ref: https://en.wikipedia.org/wiki/Euclidean_distance
-  function calculateDistance(vertex: THREE.Vector3) {
-    // Calculate Euclidean distance between current vertex and position
-    const distance = Math.sqrt(
-      Math.pow(vertex.x - position.x, 2) +
-        Math.pow(vertex.y - position.y, 2) +
-        Math.pow(vertex.z - position.z, 2)
-    );
-    return distance;
+  return { index: closestVertex, distance: minDistance };
+}
+
+// Calculates the Euclidean distance between two points.
+// ref: https://en.wikipedia.org/wiki/Euclidean_distance
+function calculateDistance(vertex: THREE.Vector3, position: THREE.Vector3) {
+  // Calculate Euclidean distance between current vertex and position
+  const distance = Math.sqrt(
+    Math.pow(vertex.x - position.x, 2) +
+      Math.pow(vertex.y - position.y, 2) +
+      Math.pow(vertex.z - position.z, 2)
+  );
+  return distance;
+}
+
+/**
+ * Find the closest two vertices from the list of vertices from a point.
+ * This can be used to find a group of 3 vertices that can be used to create a triangle.
+ *
+ * @param vertices An array of Vector3 objects representing a vertex in 3D space.
+ * @param position A Vector3 object representing a point in 3D space.
+ * @returns Array of closest and second closest vertices. [closest, second closest]
+ */
+export function findClosestTwoVertices(
+  vertices: THREE.Vector3[],
+  position: THREE.Vector3
+): [number, number] {
+  const closestVertexIndex = findClosestVertex(vertices, position);
+
+  if (closestVertexIndex === null) {
+    return [-Infinity, -Infinity]; // If no closest vertex, return null for both
   }
+
+  // Remove the closest vertex by setting it very far away.
+  const originalClosest = vertices[closestVertexIndex.index];
+  vertices[closestVertexIndex.index] = new THREE.Vector3(
+    Infinity,
+    Infinity,
+    Infinity
+  );
+
+  const secondClosestVertexIndex = findClosestVertex(vertices, position);
+  // Restore the original closest vertex
+  vertices[closestVertexIndex.index] = originalClosest;
+
+  return [closestVertexIndex.index, secondClosestVertexIndex.index];
+}
+
+/**
+ * Returns an array of vertices which have the same distance to the vertex at the current position.
+ *
+ * @param vertices List of all vertices.
+ * @param position Position we want to check from.
+ * @returns Number array containing the index of the vertices which have the same distance.
+ */
+export function checkForVerticesWithSameDistance(
+  vertices: THREE.Vector3[],
+  position: THREE.Vector3
+): number[] {
+  const { distance: minDistance } = findClosestVertex(vertices, position);
+  const sameDistance: number[] = [];
+  const vertexes: THREE.Vector3[] = [];
+
+  for (let i = 0; i < vertices.length; i++) {
+    const distance = calculateDistance(vertices[i], position);
+    if (distance === minDistance && !isVector3InList(vertices[i], vertexes)) {
+      sameDistance.push(i);
+      vertexes.push(vertices[i]);
+    }
+  }
+  return sameDistance;
 }
 
 /**
  * Computes the normal vector of a triangle formed by 3 vertices.
  * ref: https://www.khronos.org/opengl/wiki/Calculating_a_Surface_Normal
- * 
+ *
  * @param vertex1 - First vertex of the triangle
  * @param vertex2 - Second vertex of the triangle
  * @param vertex3 = Third vertex of the triangle
  * @returns Vector3 representing the normal vector.
  */
-export function computeNormal(vertex1: THREE.Vector3, vertex2: THREE.Vector3, vertex3: THREE.Vector3): THREE.Vector3 {
-    // We must compute two vectors and find their cross product to find a normal.
+export function computeNormal(
+  vertex1: THREE.Vector3,
+  vertex2: THREE.Vector3,
+  vertex3: THREE.Vector3
+): THREE.Vector3 {
+  // We must compute two vectors and find their cross product to find a normal.
 
-    // Vector AB
-    const AB = new THREE.Vector3(
-        vertex2.x - vertex1.x,
-        vertex2.y - vertex1.y,
-        vertex2.z - vertex1.z
-    );
+  // Vector AB
+  const AB = new THREE.Vector3(
+    vertex2.x - vertex1.x,
+    vertex2.y - vertex1.y,
+    vertex2.z - vertex1.z
+  );
 
-    // Vector AC
-    const AC = new THREE.Vector3(
-        vertex3.x - vertex1.x,
-        vertex3.y - vertex1.y,
-        vertex3.z - vertex1.z
-    );
-    // If we get the cross product of AB and AC will give us the normal.
-    const normal = new THREE.Vector3();
+  // Vector AC
+  const AC = new THREE.Vector3(
+    vertex3.x - vertex1.x,
+    vertex3.y - vertex1.y,
+    vertex3.z - vertex1.z
+  );
+  // If we get the cross product of AB and AC will give us the normal.
+  const normal = new THREE.Vector3();
 
-    normal.crossVectors(AB, AC);
+  normal.crossVectors(AB, AC);
 
-    return normal;
+  return normal;
+}
 
+/**
+ * Checks if a Vector3 is inside a list of Vector3s.
+ * 
+ * @param vector Vector to search for.
+ * @param list List of vectors
+ * @returns boolean - true if vector is inside, false if it is not.
+ */
+export function isVector3InList(
+  vector: THREE.Vector3,
+  list: THREE.Vector3[]
+): boolean {
+  for (let i = 0; i < list.length; i++) {
+    if (vector.equals(list[i])) {
+      return true; // The vector is in the list
+    }
+  }
+  return false; // The vector is not in the list
+}
+
+
+/**
+ * Generates combinations of a given array with a specified combination size.
+ * 
+ * @param array - The array of numbers to generate combinations from.
+ * @param combinationSize - The size of each combination.
+ * @returns An array of number arrays representing the generated combinations.
+ */
+export function generateCombinations(
+  array: number[],
+  combinationSize: number
+): number[][] {
+  const results: number[][] = [];
+
+  function combine(tempArray: number[], start: number, remaining: number) {
+    if (remaining === 0) {
+      results.push([...tempArray]);
+      return;
+    }
+
+    for (let i = start; i <= array.length - remaining; i++) {
+      tempArray.push(array[i]);
+      combine(tempArray, i + 1, remaining - 1);
+      tempArray.pop();
+    }
+  }
+
+  combine([], 0, combinationSize);
+  return results;
 }
