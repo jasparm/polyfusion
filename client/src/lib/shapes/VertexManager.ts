4945c11840a78029572e9b4a9b713c54900d31cb
Shannon Johnson <sjoh0041@student.monash.edu>
2024-09-10 16:23:57 +1000

    Add ability to select vertices in vertex manager

--- client/src/lib/shapes/VertexManager.ts
+++ client/src/lib/shapes/VertexManager.ts
@@ -3,13 +3,15 @@ import * as THREE from "three";
 export class VertexManager {
   vertexMap: Map<number, THREE.Vector3>;
   connections: number[][];
+  selectedVertices: number[]; // this stores a list of all the currently selected vertices
 
   id: number;
   groupID: string;
   constructor(groupID: string) {
     this.groupID = groupID;
     this.vertexMap = new Map();
-    this.id = 0
+    this.id = 0;
+    this.selectedVertices = [];
   }
 
   init(vertices: number[], connections: number[][]) {
@@ -44,10 +46,54 @@ export class VertexManager {
 
   add(vertex: THREE.Vector3) {
     this.vertexMap.set(this.id, vertex);
-    this.id ++;
+    this.id++;
   }
 
   getVertexFromID(vertexID: number) {
     return this.vertexMap.get(vertexID);
+  }
+
+  /**
+   * Selects a new vertex - unselects all previously selected vertices.
+   * @param vertexID
+   */
+  selectVertex(vertexID: number) {
+    if (this.checkVertexPresence(vertexID)) {
+      this.selectedVertices = [vertexID];
+    }
+  }
+
+  /**
+   * Adds a new vertex to the list of selected vertices.
+   * @param vertexID
+   */
+  addSelectedVertex(vertexID: number) {
+    if (this.checkVertexPresence(vertexID)) {
+      this.selectedVertices.push(vertexID);
+    }
+  }
+
+  /**
+   * Returns true if this vertex manager has that vertex ID, else return false.
+   * @param vertexID
+   * @returns
+   */
+  checkVertexPresence(vertexID: number): boolean {
+    if (!this.vertexMap.has(vertexID)) {
+      // do nothing, this error is not part of this shape/vertex manager
+      console.warn(vertexID + " is not found in vertex manager " + this.id);
+      return false;
+    }
+
+    return true;
+  }
+
+  colourSelectedVertices() {
+    this.selectedVertices.forEach((vertexID) => {
+      const vertex = this.getVertexFromID(vertexID);
+      console.log(vertex);
+
+      // vertex.material.color = new THREE.Color(100, 100, 100);
+    });
   }
 }
