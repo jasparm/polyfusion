eef983adc61a5973478d9895e1300538bb635ce0
jasparm <me@jaspar.dev>
2024-10-07 21:11:29 +1100

    experimenting with http automation

---
+++ test/server/test2.fish
@@ -0,0 +1,42 @@
+#!/usr/bin/fish
+
+# Base URL for the server
+set url "http://localhost:3000"
+
+# Test user details
+set user "test"
+set pass "test"
+
+# Test: Sign up a new user
+echo "Testing /signup"
+http -b POST "$url/signup" user="$user" pass="$pass"
+
+# Test: Login and capture token
+echo "Testing /login"
+set login_response (http -b POST "$url/login" user="$user" pass="$pass")
+set token (echo $login_response | jq -r '.token')
+
+# Test: Store a shape
+echo "Testing /storeshape"
+set shape_data type="2D" name="stupid" data={} image="img"
+http -v -A bearer -a $token POST "$url/storeshape" $shape_data
+
+# Test: Get shapes (2D example)
+echo "Testing /shapes/2D"
+http -b -A bearer -a $token GET "$url/shapes/2D"
+
+# Test: Get a specific shape by name
+echo "Testing /shape/2D/stupid"
+http -b -A bearer -a $token GET "$url/shape/2D/stupid" 
+
+# Test: Update the shape
+echo "Testing /updateshape:stupid"
+set updated_shape type="2D" name="stupid" data={} image="img_ahhhh"
+http -b -A bearer -a $token PUT "$url/updateshape:stupid" $updated_shape
+
+echo "Testing /shape/2D/stupid"
+http -b -A bearer -a $token GET "$url/shape/2D/stupid" 
+
+# Test: Delete the shape
+# echo "Testing /deleteshape:circle"
+# http -b POST "$url/deleteshape:circle" Authorization:"Bearer $token"
